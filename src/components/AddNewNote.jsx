f5580bb2d6751e07aec5ab6ba6b56898511582e4
Salar Gholami <salargholami021@gmail.com>
2023-10-23 03:49:24 +0330

    controlled component or controlled element in react.js

--- src/components/AddNewNote.jsx
+++ src/components/AddNewNote.jsx
@@ -1,14 +1,40 @@
+import { useState } from "react";
+
 function AddNewNote() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    const newNote = {
+      title,
+      description,
+      id: Date.now(""),
+      completed: false,
+      createdAt: new Date().toISOString(),
+    };
+
+    setTitle("");
+    setDescription("");
+
+    console.log(newNote);
   };
 
   return (
     <div className="add-new-note">
       <h2>Add New Note</h2>
       <form className="note-form" onSubmit={handleSubmit}>
-        <input type="text" className="text-field" placeholder="Note title ..." />
         <input
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+          type="text"
+          className="text-field"
+          placeholder="Note title ..."
+        />
+        <input
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
           type="text"
           className="text-field"
           placeholder="Note description ..."
