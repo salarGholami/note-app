4e479fbcf4b77472a035fc001dd45545a3b81f18
Salar Gholami <salargholami021@gmail.com>
2023-10-30 17:55:05 +0330

    Toggle Note to Complete

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,26 @@ function App() {
 
   const handleDeleteNote = (id) => {
     // const filteredNotes = notes.filter((n) => n.id !== id);
-    // setNotes(filteredNotes);
+    // setNotes(filteredNotes);   // maybye to be muted
 
-    setNotes((prevNotes) => prevNotes.filter((n) => n.id !== id));
+    setNotes((prevNotes) => prevNotes.filter((n) => n.id !== id)); //another way to unmuted react to delete notes
+  };
+
+  const handleCompleteNote = (e) => {
+    console.log(e.target.value);
+    const noteId = Number(e.target.value);
+
+    // const newNotes = notes.map((note) =>
+    //   note.id === noteId ? { ...note, completed: !note.completed } : note
+    // );
+    // setNotes(newNotes);  // maybye to be muted
+
+    setNotes(
+      (prevNotes) =>
+        prevNotes.map((note) =>
+          note.id === noteId ? { ...note, completed: !note.completed } : note
+        ) //another way to unmuted react when checkedBox notes
+    );
   };
 
   return (
@@ -23,7 +40,11 @@ function App() {
       <div className="note-app">
         <AddNewNote onAddNote={handleAddNotes} />
         <div className="note-container">
-          <NoteList notes={notes} onDelete={handleDeleteNote} />
+          <NoteList
+            notes={notes}
+            onDelete={handleDeleteNote}
+            onComplete={handleCompleteNote}
+          />
         </div>
       </div>
     </div>
