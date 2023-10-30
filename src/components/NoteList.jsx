84f5ac37ff32fcc7dc4bc4a67c6c23cc69831979
Salar Gholami <salargholami021@gmail.com>
2023-10-30 04:58:29 +0330

    NoteList markup

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -2,7 +2,7 @@ function NoteList({ notes }) {
   return (
     <div>
       {notes.map((note) => (
-        <NoteItem key={note.id} />
+        <NoteItem key={note.id} note={note} />
       ))}
     </div>
   );
@@ -10,8 +10,28 @@ function NoteList({ notes }) {
 
 export default NoteList;
 
-function NoteItem() {
-  return <div className="note-item">
-    
-  </div>;
+function NoteItem({ note }) {
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  return (
+    <div className="note-item">
+      <div className="note-item__header">
+        <div>
+          <p className="title">{note.title}</p>
+          <p className="desc">{note.description}</p>
+        </div>
+        <div className="action">
+          <button>close</button>
+          <input type="checkbox" name="" id="" />
+        </div>
+      </div>
+      <div className="note-item__footer">
+        {new Date(note.createdAt).toLocaleDateString("en-Us", options)}
+      </div>
+    </div>
+  );
 }
